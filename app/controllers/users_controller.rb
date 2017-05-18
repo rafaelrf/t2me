c18efa95a257b357c5e3f2f466540cb04b97229b
Rafael Fontenele <rafaelrfontenele@gmail.com>
2017-05-18 18:04:44 -0300

    Update Users Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,18 +13,27 @@ class UsersController < ApplicationController
   end
 
   def edit
-
+    @user = User.find(params[:id])
   end
 
   def create
     @user = User.new(user_params)
 
-    @user.save
-    redirect_to @user
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
-  def edit
+  def update
+    @user = User.find(params[:id])
 
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   def destroy
