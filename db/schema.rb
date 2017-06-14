13844627eef47777ae185afbf4df2f263804ee64
Rafael Fontenele <rafaelrfontenele@gmail.com>
2017-06-14 11:20:42 -0300

    Creating model Profiles

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170518173901) do
+ActiveRecord::Schema.define(version: 20170614141303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "profiles", force: :cascade do |t|
+    t.text "resume"
+    t.text "interest"
+    t.string "goals"
+    t.text "languages"
+    t.string "photo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
